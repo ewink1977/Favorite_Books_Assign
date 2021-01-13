d8eb8b68146c06e3409607720a07f88e3696bc7d
Erin Winking <ewink@yahoo.com>
2021-01-13 15:17:28 -0800

    MODELS made, MIGRATIONS done.

--- Favorite_Books/models.py
+++ Favorite_Books/models.py
@@ -1,3 +1,48 @@
 from django.db import models
+import re
 
-# Create your models here.
+from django.db.models.deletion import CASCADE
+
+class ValidationTest(models.Manager):
+    def basic_validation(self, postData):
+        errors = {}
+        if len(postData['firstname']) < 2:
+            errors['firstname'] = "First name must be more than two characters."
+        if len(postData['lastname']) < 2:
+            errors['lastname'] = "Last name must be more than two characters."
+        EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
+        if not EMAIL_REGEX.match(postData['email']):
+            errors['email'] = "Invalid email address!"
+        emaildupcheck = Users.objects.filter(email=postData['email'])
+        if emaildupcheck == postData['email']:
+            errors['email'] = "That email address already exists. Did you forget your password?"
+        if len(postData['password']) < 8:
+            errors['password'] = "Your password must be at least 8 characters."
+        if not postData['confirm-pw'] == postData['password']:
+            errors['confirm-pw'] = "Password mismatch. Maybe a typo? Please try again!"
+        return errors
+
+class Users(models.Model):
+    first_name = models.CharField(max_length = 100)
+    last_name = models.CharField(max_length = 100)
+    email = models.EmailField()
+    password = models.CharField(max_length = 255)
+    created_at = models.DateTimeField(auto_now_add = True)
+    updated_at = models.DateTimeField(auto_now = True)
+    # fav_books = Books the user has favorited.
+    # books_added = Books the user has uploaded/added.
+
+    objects = ValidationTest()
+
+class Books(models.Model):
+    title = models.CharField(max_length = 255)
+    description = models.TextField()
+    uploaded_by = models.ForeignKey(
+        Users,
+        related_name = "books_added",
+        on_delete = CASCADE
+    )
+    users_who_fav = models.ManyToManyField(
+        Users,
+        related_name = "fav_books",
+    )
--- readme.md
+++ readme.md
@@ -4,11 +4,11 @@ Coding Dojo's Favorite Books Assignment. As well, my first attempt at using GITI
 
 Goals:
 
->> Create the User and Book models with all appropriate relationships
->> Incorporate a validated login/registration page  
->> On the main page, allow the user to add a new book, with validations. Added books should automatically be favorited by the logged in user.  
->> Have a list of all the books on the main page displaying the title and the user who uploaded the book  
->> When the title is clicked, display a page with the book's information, including a list of all users who have favorited that book  
->> If the logged in user has favorited the book, they should be able to "un-favorite" the book  
->> If the logged in user has not yet favorited the book, there should be a link to add this book to their favorites  
->> If the logged in user is the uploader of the book, allow them to edit (same validations apply) or delete the book
+* Create the User and Book models with all appropriate relationships
+* Incorporate a validated login/registration page  
+* On the main page, allow the user to add a new book, with validations. Added books should automatically be favorited by the logged in user.  
+* Have a list of all the books on the main page displaying the title and the user who uploaded the book  
+* When the title is clicked, display a page with the book's information, including a list of all users who have favorited that book  
+* If the logged in user has favorited the book, they should be able to "un-favorite" the book  
+* If the logged in user has not yet favorited the book, there should be a link to add this book to their favorites  
+* If the logged in user is the uploader of the book, allow them to edit (same validations apply) or delete the book
